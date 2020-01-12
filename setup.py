1ec5cb2d73fb74406dc69bcf5e0800a608ad8251
Fantin Bibas <fantin.bibas@epitech.eu>
2020-01-12 17:06:45 +0000

    Add library installation files

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+
+setuptools.setup(
+    name='HCLW',
+    version='0.0.1',
+    description='Harpokrat Cryptographic Library Python Wrapper',
+    author='Fantin Bibas',
+    author_email='fantin.bibas@epitech.eu',
+    url='https://www.harpokrat.com',
+    packages=setuptools.find_packages()
+)
